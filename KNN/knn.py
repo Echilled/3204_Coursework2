147e79b839e5fe5a24aaa05f6c5dddd55293363c
Eugene Chew <echew140899@gmail.com>
2022-11-05 12:37:17 +0800

    updates

--- KNN/knn.py
+++ KNN/knn.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import sklearn
 from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
 import numpy as p
 
 
@@ -9,9 +11,20 @@ def readlogFile(file):
     return df
 
 
+def format_columns_preprocessing(dataframe):
+    le = preprocessing.LabelEncoder()
+    for column in dataframe.columns:
+        dataframe[column] = le.fit_transform(dataframe[column])
+
+
 def main():
-    dataframe = readlogFile('../T1595ActiveScanning_logs/T1595_PacketbeatTraffic_clean_Gp14_EugeneChew.csv')
-    print(dataframe)
+    dataframe = readlogFile('tailored_logs/T1595_PacketbeatTraffic_clean_Gp14_EugeneChew.csv')
+    format_columns_preprocessing(dataframe)
+
+    # Standardize variables using scaling #
+    scaler = StandardScaler()
+    scaler.fit(dataframe.drop('Binary', axis=1))
+    # scaled_features = scaler.transform(dataframe.drop('Binary', axis=1))
 
 
 if __name__ == "__main__":
