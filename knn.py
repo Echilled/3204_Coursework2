28741756660c28563c7349a19c509c66f426d6ce
Eugene Chew <echew140899@gmail.com>
2022-11-05 10:57:00 +0800

    Update

---
+++ knn.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import sklearn
+from sklearn.neighbors import NearestNeighbors
+import numpy as p
+
+
+def readlogFile(file):
+    df = pd.read_csv(file)
+    return df
+
+
+def main():
+    dataframe = readlogFile('T1595ActiveScanning_logs/T1595_PacketbeatTraffic_clean_Gp14_EugeneChew.csv')
+    print(dataframe)
+
+
+if __name__ == "__main__":
+    main()
