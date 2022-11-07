f356766ef586ac83101c07e415060ee0eb39aa09
How Shun Han <46297054+diddykong69@users.noreply.github.com>
2022-11-07 17:20:48 +0800

    Choosing optimal k value
    Generated results of different k value on a graph to determine optimal k value

--- KNN/Shun Han/knn2.py
+++ KNN/Shun Han/knn2.py
@@ -11,6 +11,8 @@ from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import classification_report
 
 import math
+
+import matplotlib.pyplot as plt
 # Importing data to a pandas DataFrame
 df = pd.read_csv("conbined_t1595_t1046.csv", thousands=',')
 
@@ -21,21 +23,30 @@ df['event.duration'] = df['event.duration'].astype(np.float64)
 
 df = df.select_dtypes(include=np.number)
 
-print(df.columns)
-
 x = df.iloc[:, 1:8]
 y = df.iloc[:, 8]
-
-df.to_csv("unused columns removed.csv")
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0, test_size=0.3)
 sc_x = StandardScaler()
 x_train = sc_x.fit_transform(x_train)
 x_test = sc_x.transform(x_test)
 
-classifier = KNeighborsClassifier(n_neighbors=19, p=2, metric='euclidean')
-classifier.fit(x_train, y_train)
+# x_plot = np.arange(1, int(math.sqrt(len(y_test)))+1).tolist()
+x_plot = np.arange(1, int(math.sqrt(len(y_test)))+1).tolist()
+y_plot = []
 
-y_pred = classifier.predict(x_test)
+for i in range(int(math.sqrt(len(y_test)))):
+    classifier = KNeighborsClassifier(n_neighbors=i+1, p=2, metric='euclidean')
+    classifier.fit(x_train, y_train)
+
+    y_pred = classifier.predict(x_test)
+
+    y_plot.append(f1_score(y_test, y_pred))
+
+
+plt.plot(x_plot, y_plot)
+plt.grid()
+plt.show()
+
 
 print(f"{classification_report(y_test, y_pred)}")
